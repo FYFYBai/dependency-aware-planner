3b0838acb0dc74968be6266a510c73e017422727
FYFYBai <93832388+FYFYBai@users.noreply.github.com>
2025-09-19 09:37:59 -0400

    bug fix

--- backend/src/main/java/com/example/demo/repository/TaskRepository.java
+++ backend/src/main/java/com/example/demo/repository/TaskRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByListId(Long listId);
     List<Task> findByProjectId(Long projectId);
     int countByListId(Long listId);
-
+    @Query("SELECT MAX(t.position) FROM Task t WHERE t.list.id = :listId")
+    Integer findMaxPositionByListId(Long listId);
 }
